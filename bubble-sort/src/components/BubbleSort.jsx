aea9d076f6d16fc8864b2a8bb659915dcd46fc72
HarshaMaria <harsha.maria@edstem.com>
2023-12-28 18:50:50 +0530

    Made Changes

--- bubble-sort/src/components/BubbleSort.jsx
+++ bubble-sort/src/components/BubbleSort.jsx
@@ -1,74 +1,3 @@
-// import React, { useState } from 'react';
-// import Visualization from './Visualization';
-
-// const BubbleSort = () => {
-//   const [inputNumbers, setInputNumbers] = useState('');
-//   const [numbers, setNumbers] = useState([]);
-//   const [sortedIndices, setSortedIndices] = useState([]);
-//   const [isSorting, setIsSorting] = useState(false);
-//   const [comparingIndices, setComparingIndices] = useState([]); 
-
-//   const handleInputChange = (event) => {
-//     setInputNumbers(event.target.value);
-//   };
-
-//   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-//   const bubbleSort = async () => {
-//     setIsSorting(true);
-//     const arr = inputNumbers.split(',').map(Number);
-//     const indices = [];
-//     const comparingIndices = [];
-
-//     for (let i = 0; i < arr.length - 1; i++) {
-//       for (let j = 0; j < arr.length - 1 - i; j++) {
-//         comparingIndices.push(j, j + 1);
-//         if (arr[j] > arr[j + 1]) {
-//           const temp = arr[j];
-//           arr[j] = arr[j + 1];
-//           arr[j + 1] = temp;
-//         }
-//         setNumbers([...arr]);
-//         setComparingIndices([...comparingIndices]);
-//         await delay(500); 
-//         comparingIndices.splice(0, comparingIndices.length);
-//       }
-//       indices.push(arr.length - 1 - i);
-//       setSortedIndices([...indices]);
-//     }
-//     setIsSorting(false);
-//   };
-
-//   return (
-//     <div>
-//       <h1 style={{ marginLeft: '636px', marginBottom: '24px', fontSize: '40px' }}>Bubble Sort App</h1>
-//       <div style={{ marginLeft: '600px' }}>
-//         <label style={{ marginBottom: '12px', fontSize: '24px' }}>
-//           Enter Numbers:-
-//           <input
-//             type="text"
-//             value={inputNumbers}
-//             onChange={handleInputChange}
-//             style={{ marginLeft: '40px', padding: '5px', borderRadius: '3px', border: '1px solid' }}
-//           />
-//         </label>
-//       </div>
-//       <button style={{ marginLeft: '720px', marginTop: '12px' }} onClick={bubbleSort} disabled={isSorting}>
-//         {isSorting ? 'Sorting...' : 'Sort Numbers'}
-//       </button>
-//       <Visualization numbers={numbers} sortedIndices={sortedIndices} comparingIndices={comparingIndices} />
-//     </div>
-//   );
-// };
-
-// export default BubbleSort;
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import Visualization from './Visualization';
 
@@ -78,7 +7,7 @@ const BubbleSort = () => {
   const [sortedIndices, setSortedIndices] = useState([]);
   const [isSorting, setIsSorting] = useState(false);
   const [comparingIndices, setComparingIndices] = useState([]);
-  const [sortOrder, setSortOrder] = useState('ascending'); // Default to ascending
+  const [sortOrder, setSortOrder] = useState('ascending');
 
   const handleInputChange = (event) => {
     setInputNumbers(event.target.value);
@@ -114,9 +43,9 @@ const BubbleSort = () => {
     }
 
     if (sortOrder === 'ascending') {
-      setNumbers([...arr]); // Set the array in ascending order
+      setNumbers([...arr]); 
     } else {
-      setNumbers([...arr].reverse()); // Set the array in descending order
+      setNumbers([...arr].reverse()); 
     }
 
     setIsSorting(false);
